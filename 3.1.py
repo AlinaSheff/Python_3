3eb3f4955857fa769ec96bc68018acafe212dacf
Alina <alinasheff@mail.ru>
2022-12-21 16:55:09 +0200

    Добавила задачу 3.1

---
+++ 3.1.py
@@ -0,0 +1,14 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+from random import randint
+list = []
+for i in range(5):
+    list.append(randint(1, 10))
+print(list)
+sum = 0
+for i in range(len(list)-1):
+    if i % 2 != 0:
+        sum += list[i]
+print(sum)
